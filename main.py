333a74fc4af68c34dc3eec7dd55abda4f48c6d18
koohack <qkrpsh97@naver.com>
2023-03-04 16:12:27 +0000

    feat: tsimple tokenizer

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.optim import AdamW
 from torch.nn.utils.rnn import pad_sequence
 from tqdm import tqdm
 from torch.nn.parallel import DistributedDataParallel as DDP
+from tokenizer import Tokenizer
 from transformers import (
     BertTokenizer, 
     BertConfig, 
@@ -34,21 +35,23 @@ class Config():
     lr = 5e-5
     batch_size = 4
     num_steps = 1024
-    
+    max_length = 10240
     
     ## 2. mid
 
     
     ## 3. low
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    log_dir = "./logs"
     
 
 if __name__ == "__main__":
     set_seed(6)
     config = Config
     
-    
-    
+    tokenizer = Tokenizer()
+    print(tokenizer.encode("te"))
+    print(tokenizer.decode([1,2]))
     
     
     
---
+++ tokenizer.py
@@ -0,0 +1,51 @@
+class Tokenizer():
+    def __init__(self):
+        self.encode_tokenizer, self.decode_tokenizer = self.make_tokenizer()
+        self.mask = "*"
+    
+    def make_tokenizer(self):
+        store = []
+        init_small = ord("a")
+        init_capital = ord("A")
+        
+        for i in range(26):
+            store.append(chr(init_small + i))
+            store.append(chr(init_capital + i))
+        
+        store.append(" ")
+        store.append(".")
+        store.append(",")
+        store.append("'")
+        store.append("!")
+        store.append("?")
+        store.append("-")
+        store.append("+")
+
+        encode_tokenizer = dict()
+        for i in range(len(store)):
+            encode_tokenizer[store[i]] = i
+
+        decode_tokenizer = dict()
+        for i in range(len(store)):
+            decode_tokenizer[i] = store[i]
+        
+        return encode_tokenizer, decode_tokenizer
+    
+        
+    def encode(self, text):
+        out = []
+        text = "I love you!"
+        for item in text:
+            out.append(self.encode_tokenizer[item])
+        return out
+    
+    def decode(self, text_list):
+        out = []
+        text_list = [1, 3, 5, 6, 23, 4, 12]
+        for item in text_list:
+            out.append(self.decode_tokenizer[item])
+        
+        return "".join(out)
+
+            
+        
